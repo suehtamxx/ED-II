8c736952caad58d5848685d6da19803a2c9ae0cf
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-09-27 23:17:05 -0300

    Update universidade.h

--- universidade.h
+++ universidade.h
@@ -71,18 +71,46 @@ typedef struct lista_alunos
    struct lista_alunos *prox;
 }l_aluno;
 
-l_aluno *cadastrar_aluno(arv_curso *curso);
-arv_curso *cadastrar_curso(); // criar um nó  na arvore curso
-arv_dis *cadastrar_disciplina(arv_curso *curso);
-void cadastrar_matricula(arv_matri matricula, struct lista_alunos *l_aluno);
-void cadastrar_notas(arv_notas notas, arv_matri matricula, struct lista_alunos *l_aluno);
-
+//Criar árvores e lista
 arv_curso *criar_arv_curso();
 arv_dis *criar_arv_dis();
-l_aluno * criar_list_aluno();
+arv_matri *criar_arv_matricula();
+arv_notas *criar_arv_notas();
+l_aluno * criar_lista_aluno();
 
+//Criar os nós das árvores e lista
+arv_curso *alocar_no_curso();
+arv_dis *alocar_no_dis();
+arv_matri *alocar_no_matricula();
+arv_notas *alocar_no_notas();
+l_aluno *alocar_no_aluno();
+
+//Preencher as structs
+arv_curso *cadastrar_curso(arv_curso **no);
+arv_dis *cadastrar_disciplina(arv_dis **no, arv_curso *curso);
+// arv_matri cadastrar_matricula(arv_matri matricula, struct lista_alunos *l_aluno);
+// arv_notas cadastrar_notas(arv_notas notas, arv_matri matricula, struct lista_alunos *l_aluno);
+l_aluno *cadastrar_aluno(l_aluno **no, arv_curso *curso);
+
+//Inserir nas árvores e lista
 int inserir_arv_curso(arv_curso **curso, arv_curso *no);
 int inserir_arv_dis(arv_dis **disciplina, arv_dis *no);
-int inserir_aluno(l_aluno **aluno, l_aluno *no);
+int inserir_lista_aluno(l_aluno **aluno, l_aluno *no);
+
+//Imprimir as árvores e lista (in-ordem)
+void imprimir_arv_curso(arv_curso *curso);
+void imprimir_arv_dis(arv_dis *raiz);
+void imprimir_lista_aluno(l_aluno *no);
+
+
+//Buscar nós nas árvores e lista
+
+
+
+//Remover nós nas árvores e lista
+
+
+//Liberar memória
+
 
 #endif
